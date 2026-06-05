ab766b42167e378dfaa99b7ae71c8322ba75263d
sobirjonqadirov <sobirjon.dev@gmail.com>
2026-06-05 21:52:00 +0500

    alpha-1.0: add config and update server stub

--- src/config.py
+++ src/config.py
@@ -1,1 +1,12 @@
- 
+import os
+
+APP_NAME    = "CinderBatt"
+VERSION     = "alpha-1.0"
+BASE_DIR    = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+ASSETS_DIR  = os.path.join(BASE_DIR, "assets")
+SERVER_PATH = os.path.join(BASE_DIR, "server", "version.json")
+
+ICON_TRAY       = os.path.join(ASSETS_DIR, "icon-transparent.png")
+ICON_NORMAL     = os.path.join(ASSETS_DIR, "icon.png")
+ICON_INVERSE    = os.path.join(ASSETS_DIR, "icon-inverse.png")
